af997e5ab9d5d132f383a42c8c973492aa915cff
redtail <hongwei0522@gmail.com>
2022-05-10 18:04:44 +0800

    config

---
+++ _nuxt/static/1652177012/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFrontEnter\u002F_nuxt\u002Fstatic\u002F1652177012",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FFrontEnter\u002F",assetsPath:"\u002FFrontEnter\u002F_nuxt\u002F",cdnURL:a}}}}(null));
