bf78f041ca219a0f2acff2da27d5c97cc574f558
redtail <hongwei0522@gmail.com>
2022-05-12 09:41:46 +0800

    share-link-content

--- nuxt/static/1652319296/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fsouthern","\u002Feverywhere","\u002F","\u002Fnorthern"]})
---
+++ nuxt/static/1652319553/southern/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFrontEnter\u002Fnuxt\u002Fstatic\u002F1652319553",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsouthern",config:{_app:{basePath:"\u002FFrontEnter\u002F",assetsPath:"\u002FFrontEnter\u002Fnuxt\u002F",cdnURL:a}}}}(null));
